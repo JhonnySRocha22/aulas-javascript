38d75c850dae93e4b738b54cb516ea498be72032
JhonnySRocha22 <joaovictor200152@gmail.com>
2023-10-05 16:49:43 -0300

    Exercício 01 no JS

--- js/02-estruturas-de-dados.js
+++ js/02-estruturas-de-dados.js
@@ -11,5 +11,12 @@ Listan de dados não indexados e formados por propriedades e valores.
 let cursos = ["Node.js", "React", "UX/UI Design", "SQL"];
 
 
-
+console.log(cursos); // saída direta/estruturada (APENAS NO CONSOLE)
 console.log(cursos[3]); //SQL
+
+
+// Exercício
+const dados = ["Cristiano", "Souza", 32, "11965753422", "Florianópolis"];
+
+console.log(`${dados[0]} tem ${dados[2]} anos.`);
+
