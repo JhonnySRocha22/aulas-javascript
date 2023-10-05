5554dd62bc8549edda5bd06cfb441e600a8ae45c
JhonnySRocha22 <joaovictor200152@gmail.com>
2023-10-05 15:29:29 -0300

    Arrays

---
+++ js/02-estruturas-de-dados.js
@@ -0,0 +1,12 @@
+/* Estruturas de Dados 
+
+- Arrays (Vetores/Matrizes):
+Listas de dados indexados, sequênciais e acessíveis por um índice numérico 
+
+- Objetos
+Listan de dados não indexados e formados por propriedades e valores.
+*/
+
+// Arrays
+let cursos = ["Node.js", "React", "UX/UI Design", "SQL"];
+
