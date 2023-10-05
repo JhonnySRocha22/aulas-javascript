9f4522b30fedd4171bcb8e120c4d874353e16e1d
JhonnySRocha22 <joaovictor200152@gmail.com>
2023-10-05 15:03:27 -0300

    Sintaxe Geral do JS

---
+++ js/01-sintaxe-geral.js
@@ -0,0 +1,30 @@
+console.log("Programador Web");
+
+/* Comentario em bloco - ALT SHITF A */
+// Comentario em linha - CTRL ;
+
+
+
+/* Manipulação de dados */
+
+
+// Variáveis
+let curso = "HTML5 e CSS3";
+let ano = 2023;
+
+// Constantes
+const mediaMinima = 7;
+
+
+
+// Saída de dados a partir da memória
+console.log(curso);
+console.log(mediaMinima);
+console.log(ano);
+
+
+// Template String/Literal: usando crases e ${}
+console.log(`Estou fazendo o curso de ${curso} neste ano de ${ano}.`);
+
+// Concatenação (forma mais antga de saída, mas ainda válida)
+console.log("Estou fazendo o curso de "+curso+" neste ano de "+ano+".");
