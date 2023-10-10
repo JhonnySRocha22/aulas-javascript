5092214078e7bfc28b35c12a309967ac35c81338
JhonnySRocha22 <joaovictor200152@gmail.com>
2023-10-10 15:24:14 -0300

    Condicional funcional

--- js/03-condicionais.js
+++ js/03-condicionais.js
@@ -8,18 +8,19 @@ ELSE - senão
 IF ELSE - senão se*/
 
 let usuario = "Juean";
-let idade = 15;
+let idade = 25;
+let mensagem;  //indefinida/undefined
 
 //Verificar a idade e determinar se é maior ou menor
 if(idade >= 18 ){
-    console.log(`${usuario} é maior de idade!`);
+    mensagem = "maior";
 } else {
-    console.log(`${usuario} é menor de idade!`);
+    mensagem = "menor" ;
 
 }
 
-
-
+//Apresentando os resultados após a condicional
+console.log(`${usuario} é ${mensagem} de idade!`);
 
 
 
