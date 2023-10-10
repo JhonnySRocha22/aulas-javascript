2e922fce6ab7e0d84f8ed54687e73d94ca242bd7
JhonnySRocha22 <joaovictor200152@gmail.com>
2023-10-10 17:16:52 -0300

    Exercício JS

---
+++ js/exercicio01.js
@@ -0,0 +1,22 @@
+
+let nota1 = 10;
+let nota2 = 9;
+let media = (nota1+nota2)/2
+let mensagem;
+
+
+
+
+
+
+
+if(media >=7){
+    mensagem = "Aprovado(a)"
+}else if (media >= 5){
+    mensagem = "Recuperação"
+}else {
+    mensagem = "Reprovado(a)"
+}
+
+
+console.log(`O Aluno(a) está com a média ${media} e está ${mensagem}!!!`)
