5b2aedb0163ee98ea17687e639e4083fd45cc400
JhonnySRocha22 <joaovictor200152@gmail.com>
2023-10-10 15:15:47 -0300

    if e else

---
+++ js/03-condicionais.js
@@ -0,0 +1,110 @@
+/* Estruturas de Controle Condicional
+Comandos que permitem analisar uma ou mais condições.
+E a partir do resultado desta análise (verdadeiro ou falso), o programa pode executar ações diferentes. 
+
+Comandos mais comuns: 
+IF - se
+ELSE - senão
+IF ELSE - senão se*/
+
+let usuario = "Juean";
+let idade = 15;
+
+//Verificar a idade e determinar se é maior ou menor
+if(idade >= 18 ){
+    console.log(`${usuario} é maior de idade!`);
+} else {
+    console.log(`${usuario} é menor de idade!`);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
