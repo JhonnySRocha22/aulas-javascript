33013cf949015ccc75524b6d539f6f9b3b8ac69a
JhonnySRocha22 <joaovictor200152@gmail.com>
2023-10-17 14:08:39 -0300

    Exercício 02

---
+++ js/exercicio02.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const alunos = [
+
+{
+    id: 1,
+    nome: "Joãozinho"
+},
+
+{
+    id: 2,
+    nome: "Mariazinha"
+},
+
+{
+    id: 3,
+    nome: "Giuseppezinho"
+}
+
+]
+
+
+let quantidadedeAlunos = alunos.length;
+
+for(let i = 0; i < quantidadedeAlunos; i++){
+console.log(alunos[i].nome);
+
+}
+
+console.log("--------------------------");
+
+let i = 0;
+
+while(i < quantidadedeAlunos ){
+    console.log(alunos[i].nome);
+    i++;
+}
