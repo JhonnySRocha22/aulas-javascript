a769a0e12408a2cced221787f27500713e60be76
JhonnySRocha22 <joaovictor200152@gmail.com>
2023-10-17 17:27:19 -0300

    Terminar em casa

---
+++ js/exercicio03.js
@@ -0,0 +1,63 @@
+"use strict";
+
+
+
+
+function converterReaisParaDolares(valorEmReais, taxaDeCambio) {
+    const valorEmDolares = valorEmReais / taxaDeCambio;
+    return valorEmDolares;
+  }
+
+  const taxaDeCambio = 5.03;
+
+  const valorEmReais = 5000;
+
+  const valorEmDolares = converterReaisParaDolares(valorEmReais, taxaDeCambio);
+
+  console.log(`${valorEmReais} BRL é equivalente a ${valorEmDolares} USD`);
+
+
+  
+
+  const taxaDeCambio2 = 5.03;
+
+  const valorEmDolaresConvertido  = 994.03;
+
+  const valorEmDolaresConvertidos = (valorEmDolaresConvertido, taxaDeCambio2);
+
+  console.log(`${valorEmDolares} USD é equivalente a ${valorEmReais} BRL`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
